5bc4aa68dfbfecf4d4e2d3eb2f76cfa29c29f2d1
Gabriel <knuckles4x@gmail.com>
2020-05-26 04:13:08 -0300

    show track status

--- src/components/TrackingEvents/index.js
+++ src/components/TrackingEvents/index.js
@@ -8,20 +8,55 @@ export default function TrackingEvents({ track }) {
 
   return (
     <>
+      <h2>Rastreio: {track.code}</h2>
       <ul className="list-group">
         <li className="list-group-item">
-          <span>{track.code}</span>
-          <span>{track.type}</span>
-          <span>{track.postedAt}</span>
-          <span>{track.updatedAt}</span>
-          <span>{track.isDelivered}</span>
-          <ul>
-            {track.tracks.map((t) => (
-              <li key={t.trackedAt} className="list-group-item">
-                <span>{t.locale}</span>
-              </li>
-            ))}
-          </ul>
+          <span>Status: {track.isDelivered ? 'Entregue' : 'Em andamento'}</span>
+          <span>
+            Última atualização:{' '}
+            {new Intl.DateTimeFormat('pt-BR', {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric',
+            }).format(new Date(track.updatedAt))}
+          </span>
+          <span>
+            Data da postagem:{' '}
+            {new Intl.DateTimeFormat('pt-BR', {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'numeric',
+              day: 'numeric',
+              hour: 'numeric',
+              minute: 'numeric',
+            }).format(new Date(track.postedAt))}
+          </span>
+          <br />
+          <table className="table table-striped">
+            <tbody>
+              {track.tracks.map((t) => (
+                <tr key={t.trackedAt}>
+                  <td>
+                    {new Intl.DateTimeFormat('pt-BR', {
+                      year: 'numeric',
+                      month: 'numeric',
+                      day: 'numeric',
+                      hour: 'numeric',
+                      minute: 'numeric',
+                    }).format(new Date(t.trackedAt))}
+                    <br />
+                    {t.locale}
+                  </td>
+                  <td>
+                    {t.status} {t.observation}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </li>
       </ul>
     </>
